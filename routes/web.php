a0741e794fa63274430405261f2b430c7410c15a
muhammadcahya <muhammadcahyax@gmail.com>
2023-05-08 13:11:46 +0700

    Add route-list

--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,24 @@ Route::get('/artisan/{command}', function ($command) {
     $exitCode = Artisan::call($command);
     return "<u>{$command}</u> has been executed";
 })->name('artisan');
+
+Route::get('/route-list', function () {
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo "<tr>";
+    echo "<td width='10%'><h4>HTTP Method</h4></td>";
+    echo "<td width='10%'><h4>Route</h4></td>";
+    echo "<td width='10%'><h4>Name</h4></td>";
+    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+    echo "</tr>";
+    foreach ($routeCollection as $value) {
+        echo "<tr>";
+        echo "<td>" . $value->methods()[0] . "</td>";
+        echo "<td>" . $value->uri() . "</td>";
+        echo "<td>" . $value->getName() . "</td>";
+        echo "<td>" . $value->getActionName() . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+})->name('artisan');
